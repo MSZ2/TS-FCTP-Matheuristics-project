754e7aa995d09cae7c99f5a9192d422f0f07abed
MSZ2 <83076313+MSZ2@users.noreply.github.com>
2026-01-26 23:03:33 +0100

    Added cplex native types and constraints with constants on rhs

--- solve_tsfctp.cpp
+++ solve_tsfctp.cpp
@@ -2,7 +2,6 @@
 #include <ilcplex/ilocplex.h>
 #include <iostream>
 #include <iomanip>
-#include <vector>
 
 ILOSTLBEGIN
 
@@ -19,28 +18,40 @@ int main(int argc, char* argv[]) {
     }
 
     // --- UCITAVANJE INSTANCE ---
-    int I, J, K; // Broj fabrika, depoa, kupaca
-    in >> I >> J >> K;
+    IloInt I, J, K; 
+    in >> I >> J >> K;   // CHANGED: use IloInt instead of int
 
-    std::vector<double> s(I); // kapacitet fabrika
-    std::vector<double> d(K); // potraznja kupaca
-    for (int i = 0; i < I; i++) in >> s[i];
-    for (int k = 0; k < K; k++) in >> d[k];
+    IloNumArray s(I);    // CHANGED: use IloNumArray
+    IloNumArray d(K);    // CHANGED: use IloNumArray
 
-    // Troškovi transporta i fiksni troškovi
-    std::vector<std::vector<double>> b(I, std::vector<double>(J));
-    std::vector<std::vector<double>> f(I, std::vector<double>(J));
-    std::vector<std::vector<double>> c(J, std::vector<double>(K));
-    std::vector<std::vector<double>> g(J, std::vector<double>(K));
+    for (IloInt i = 0; i < I; i++) in >> s[i];
+    for (IloInt k = 0; k < K; k++) in >> d[k];
 
-    for (int i = 0; i < I; i++)
-        for (int j = 0; j < J; j++) in >> b[i][j];
-    for (int i = 0; i < I; i++)
-        for (int j = 0; j < J; j++) in >> f[i][j];
-    for (int j = 0; j < J; j++)
-        for (int k = 0; k < K; k++) in >> c[j][k];
-    for (int j = 0; j < J; j++)
-        for (int k = 0; k < K; k++) in >> g[j][k];
+    // CHANGED: use IloArray<IloNumArray> instead of std::vector
+    IloArray<IloNumArray> b(I), f(I);
+    IloArray<IloNumArray> c(J), g(J);
+
+    for (IloInt i = 0; i < I; i++) {
+        b[i] = IloNumArray(J);
+        f[i] = IloNumArray(J);
+    }
+
+    for (IloInt j = 0; j < J; j++) {
+        c[j] = IloNumArray(K);
+        g[j] = IloNumArray(K);
+    }
+
+    for (IloInt i = 0; i < I; i++)
+        for (IloInt j = 0; j < J; j++) in >> b[i][j];
+
+    for (IloInt i = 0; i < I; i++)
+        for (IloInt j = 0; j < J; j++) in >> f[i][j];
+
+    for (IloInt j = 0; j < J; j++)
+        for (IloInt k = 0; k < K; k++) in >> c[j][k];
+
+    for (IloInt j = 0; j < J; j++)
+        for (IloInt k = 0; k < K; k++) in >> g[j][k];
 
     in.close();
 
@@ -49,24 +60,23 @@ int main(int argc, char* argv[]) {
         IloModel model(env);
 
         // --- VARIJABLE ---
-        // Flatten 2D varijable x[i][j] u 1D za bolje performanse
         IloNumVarArray x(env, I * J, 0.0, IloInfinity, ILOFLOAT);
-        IloNumVarArray z(env, I * J, 0.0, 1.0, ILOBOOL);
+        IloBoolVarArray z(env, I * J);   // CHANGED: use IloBoolVarArray
         IloNumVarArray y(env, J * K, 0.0, IloInfinity, ILOFLOAT);
-        IloNumVarArray w(env, J * K, 0.0, 1.0, ILOBOOL);
+        IloBoolVarArray w(env, J * K);   // CHANGED: use IloBoolVarArray
 
-        auto idx_x = [J](int i, int j) { return i * J + j; };
-        auto idx_y = [K](int j, int k) { return j * K + k; };
+        auto idx_x = [J](IloInt i, IloInt j) { return i * J + j; };
+        auto idx_y = [K](IloInt j, IloInt k) { return j * K + k; };
 
         // --- OBJEKTIVNA FUNKCIJA ---
         IloExpr obj(env);
-        for (int i = 0; i < I; i++)
-            for (int j = 0; j < J; j++)
-                obj += b[i][j] * x[idx_x(i, j)] + f[i][j] * z[idx_x(i, j)];
+        for (IloInt i = 0; i < I; i++)
+            for (IloInt j = 0; j < J; j++)
+                obj += b[i][j] * x[idx_x(i,j)] + f[i][j] * z[idx_x(i,j)];
 
-        for (int j = 0; j < J; j++)
-            for (int k = 0; k < K; k++)
-                obj += c[j][k] * y[idx_y(j, k)] + g[j][k] * w[idx_y(j, k)];
+        for (IloInt j = 0; j < J; j++)
+            for (IloInt k = 0; k < K; k++)
+                obj += c[j][k] * y[idx_y(j,k)] + g[j][k] * w[idx_y(j,k)];
 
         model.add(IloMinimize(env, obj));
         obj.end();
@@ -74,77 +84,71 @@ int main(int argc, char* argv[]) {
         // --- OGRANICENJA ---
 
         // Kapacitet fabrika
-        for (int i = 0; i < I; i++) {
+        for (IloInt i = 0; i < I; i++) {
             IloExpr expr(env);
-            for (int j = 0; j < J; j++)
-                expr += x[idx_x(i, j)];
-            model.add(expr <= s[i]);
+            for (IloInt j = 0; j < J; j++)
+                expr += x[idx_x(i,j)];
+
+            model.add(expr - s[i] <= 0);  // CHANGED: constraint style
             expr.end();
         }
 
         // Zadovoljavanje potraznje kupaca
-        for (int k = 0; k < K; k++) {
+        for (IloInt k = 0; k < K; k++) {
             IloExpr expr(env);
-            for (int j = 0; j < J; j++)
-                expr += y[idx_y(j, k)];
-            model.add(expr == d[k]);
+            for (IloInt j = 0; j < J; j++)
+                expr += y[idx_y(j,k)];
+
+            model.add(expr - d[k] == 0); // CHANGED: constraint style
             expr.end();
         }
 
-        // Konzervacija toka na depou (uključuje sve dolazne i odlazne tokove)
-        for (int j = 0; j < J; j++) {
-            IloExpr in_expr(env);
-            IloExpr out_expr(env);
-            for (int i = 0; i < I; i++)
-                in_expr += x[idx_x(i, j)];
-            for (int k = 0; k < K; k++)
-                out_expr += y[idx_y(j, k)];
-            model.add(in_expr == out_expr);
-            in_expr.end();
-            out_expr.end();
+        // Konzervacija toka na depou
+        for (IloInt j = 0; j < J; j++) {
+            IloExpr expr(env);
+            for (IloInt i = 0; i < I; i++)
+                expr += x[idx_x(i,j)];
+            for (IloInt k = 0; k < K; k++)
+                expr -= y[idx_y(j,k)];
+
+            model.add(expr == 0);        // CHANGED: single expression
+            expr.end();
         }
 
-        // Link x[i][j] i z[i][j] (ako z=0, x=0)
-        for (int i = 0; i < I; i++)
-            for (int j = 0; j < J; j++)
-                model.add(x[idx_x(i, j)] <= s[i] * z[idx_x(i, j)]);
+        // Link x[i][j] i z[i][j]
+        for (IloInt i = 0; i < I; i++)
+            for (IloInt j = 0; j < J; j++)
+                model.add(x[idx_x(i,j)] - s[i] * z[idx_x(i,j)] <= 0); // CHANGED
 
-        // Link y[j][k] i w[j][k] (ako w=0, y=0)
-        for (int j = 0; j < J; j++)
-            for (int k = 0; k < K; k++)
-                model.add(y[idx_y(j, k)] <= d[k] * w[idx_y(j, k)]);
+        // Link y[j][k] i w[j][k]
+        for (IloInt j = 0; j < J; j++)
+            for (IloInt k = 0; k < K; k++)
+                model.add(y[idx_y(j,k)] - d[k] * w[idx_y(j,k)] <= 0); // CHANGED
 
         // --- SOLVE ---
         IloCplex cplex(model);
-
-        cplex.setParam(IloCplex::TiLim, 7200); // 2h limit
+        cplex.setParam(IloCplex::TiLim, 7200);
         cplex.setParam(IloCplex::Threads, 1);
-        cplex.setOut(env.getNullStream()); // ispis isključen
+        cplex.setOut(env.getNullStream());
 
         bool solved = cplex.solve();
 
         std::cout << std::fixed << std::setprecision(2);
 
-        if (solved) {
-            IloAlgorithm::Status status = cplex.getStatus();
-            double time = cplex.getTime();
-            double iters = cplex.getNiterations();
-            double nodes = cplex.getNnodes();
+        if (solved && cplex.isPrimalFeasible()) {
+            std::string statusStr =
+                (cplex.getStatus() == IloAlgorithm::Optimal) ? "OPTIMAL" : "FEASIBLE";
 
-            if (cplex.isPrimalFeasible()) {
-                double objVal = cplex.getObjValue();
-                std::string statusStr = (status == IloAlgorithm::Optimal) ? "OPTIMAL" : "FEASIBLE";
-
-                std::cout << argv[1] << " " << statusStr << " "
-                          << objVal << " " << time << " " << iters << " " << nodes << "\n";
-            } else {
-                std::cout << argv[1] << " NO_FEASIBLE - " << time << " "
-                          << iters << " " << nodes << "\n";
-            }
+            std::cout << argv[1] << " " << statusStr << " "
+                      << cplex.getObjValue() << " "
+                      << cplex.getTime() << " "
+                      << cplex.getNiterations() << " "
+                      << cplex.getNnodes() << "\n";
         } else {
             std::cout << argv[1] << " NO_FEASIBLE - "
-                      << cplex.getTime() << " " << cplex.getNiterations()
-                      << " " << cplex.getNnodes() << "\n";
+                      << cplex.getTime() << " "
+                      << cplex.getNiterations() << " "
+                      << cplex.getNnodes() << "\n";
         }
 
     } catch (IloException &e) {
